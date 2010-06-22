4f337539fd22a6aca3d9ee8a9f45e668e6cc140b
carr <tomislav@infinum.hr>
2010-06-22 12:58:57 +0200

    Plugin installation again

--- install.rb
+++ install.rb
@@ -1,5 +1,6 @@
+require 'ftools'
 puts "Copying images and stylesheets to public/ folder"
 plugin_folder = File.join(RAILS_ROOT, 'vendor', 'plugins', 'link_to_extras')
 
-File.copy File.join(plugin_folder, 'public', 'images', 'icons'), File.join(RAILS_ROOT, 'public', 'images', 'icons')
-File.copy File.join(plugin_folder, 'public', 'stylesheets', 'icons.css'), File.join(RAILS_ROOT, 'public', 'stylesheets', 'icons.css')
+FileUtils.cp_r File.join(plugin_folder, 'public', 'images'), File.join(RAILS_ROOT, 'public', 'images', 'icons')
+FileUtils.cp_r File.join(plugin_folder, 'public', 'stylesheets', 'icons.css'), File.join(RAILS_ROOT, 'public', 'stylesheets', 'icons.css')
