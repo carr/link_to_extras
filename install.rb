646a08a65519e06285aea08c1a1b9983349c1536
carr <tomislav@infinum.hr>
2010-06-22 12:46:05 +0200

    Plugin installation

---
+++ install.rb
@@ -0,0 +1,6 @@
+puts "Copying images and stylesheets to public/ folder"
+plugin_folder = File.join(RAILS_ROOT, 'vendor', 'plugins')
+
+
+File.copy File.join(plugin_folder, 'public', 'images', 'icons'), File.join(RAILS_ROOT, 'public', 'images', 'icons')
+File.copy File.join(plugin_folder, 'public', 'stylesheets', 'icons.css'), File.join(RAILS_ROOT, 'public', 'stylesheets', 'icons.css')
